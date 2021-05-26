eaa12b38202ae8d464f09c54593ff5db9f7e2e2e
Patrick Varin <patrickjvarin@gmail.com>
2021-05-26 10:30:40 -0400

    better example

--- src/example.py
+++ src/example.py
@@ -15,7 +15,7 @@ Dashboard.default().add_series(Series("x", x))
 Dashboard.default().add_series(Series("y1", y1))
 Dashboard.default().add_series(Series("y2", y2))
 Dashboard.default().add_series(Series("y3", y3))
-Dashboard.default().add_series(Series("thisisareallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallylongname", y3))
+Dashboard.default().add_series(Series("this_is_a_really_really_really_really_really_really_really_really_long_name", y3))
 
 # Add a couple of charts
 Dashboard.default().add_chart(Chart("chart1", [["x", "y1"], ["x", "y2"]]))
@@ -27,13 +27,16 @@ Dashboard.default().add_series([Series(f"example_{i}", np.linspace(0, 1)) for i 
 # Add multiple charts at once.
 Dashboard.default().add_chart([Chart(f"chart{i}", [["y2", "y3"]]) for i in range(3,10)])
 
-# x_new = np.linspace(1.1, 2.0)
-# y1_new = np.sin(np.pi * x_new)
-# y2_new = np.sin(2 * np.pi * x_new)
-# y3_new = np.sin(3 * np.pi * x_new)
-# Dashboard.default().extend_series("x", x_new)
-# Dashboard.default().extend_series("y1", y1_new)
-# Dashboard.default().extend_series("y2", y2_new)
-# Dashboard.default().extend_series("y3", y3_new)
-# Dashboard.default().update()
-# Dashboard.default().save("/tmp/test.json")
+# Extend some of the series
+x_new = np.linspace(1.1, 2.0, 100)
+y1_new = np.sin(np.pi * x_new)
+y2_new = np.sin(2 * np.pi * x_new)
+y3_new = np.sin(3 * np.pi * x_new)
+Dashboard.default().extend_series("x", x_new)
+Dashboard.default().extend_series("y1", y1_new)
+Dashboard.default().extend_series("y2", y2_new)
+Dashboard.default().extend_series("y3", y3_new)
+Dashboard.default().update()
+
+# Save the data to a json file.
+Dashboard.default().save("/tmp/test.json")
