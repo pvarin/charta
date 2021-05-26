dfe7cc10a78aa802c9a9b14e270274a6347b0556
Patrick Varin <patrickjvarin@gmail.com>
2021-05-26 16:29:17 -0400

    - Allowing the client to set the zmq port. - Moving the example to an example directory.

--- charta/client.py
+++ charta/client.py
@@ -1,8 +1,9 @@
 import zmq
 import time
 
+from charta.common import DEFAULT_ZMQ_PORT
 
-def send_data(method, protocol="tcp", port=5556, **kwargs):
+def send_data(method, protocol="tcp", port=DEFAULT_ZMQ_PORT, **kwargs):
     with zmq.Context().socket(zmq.REQ) as socket:
         socket.bind("tcp://*:{port}".format(protocol=protocol, port=port))
         kwargs["method"] = method
@@ -18,37 +19,43 @@ class Dashboard:
             cls._default = cls()
         return cls._default
 
+    def __init__(self, port=DEFAULT_ZMQ_PORT):
+        self.port = port
+
+    def send_data(self, *args, **kwargs):
+        send_data(*args, port=self.port, **kwargs)
+
     def add_chart(self, chart):
         if (type(chart) == list):
-            send_data("create_chart", data=[c.to_dict() for c in chart])
+            self.send_data("create_chart", data=[c.to_dict() for c in chart])
         else:
-            send_data("create_chart", data=chart.to_dict())
+            self.send_data("create_chart", data=chart.to_dict())
 
     def add_series(self, series):
         if (type(series) == list):
-            send_data("create_series", data=[s.to_dict() for s in series])
+            self.send_data("create_series", data=[s.to_dict() for s in series])
         else:
-            send_data("create_series", data=series.to_dict())
+            self.send_data("create_series", data=series.to_dict())
 
     def append_series(self, series_key, data):
         self.extend_series(series_key, [data])
 
     def extend_series(self, series_key, data):
-        send_data("extend_series", data=list(data), key=series_key)
+        self.send_data("extend_series", data=list(data), key=series_key)
 
     def save(self, filename):
-        send_data("read", filename=filename)
+        self.send_data("read", filename=filename)
 
     def update(self):
-        send_data("update")
+        self.send_data("update")
 
     def reset(self):
         self.delete_series()
         self.delete_charts()
 
     def delete_charts(self):
-        send_data("delete_charts")
+        self.send_data("delete_charts")
 
     def delete_series(self):
-        send_data("delete_series")
+        self.send_data("delete_series")
 
