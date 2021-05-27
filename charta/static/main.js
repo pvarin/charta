2c0fd1512c0738fdf5ce422c3982c1b16d48f869
Patrick Varin <patrickjvarin@gmail.com>
2021-05-26 22:22:37 -0400

    some formatting and functionality

--- charta/static/main.js
+++ charta/static/main.js
@@ -63,7 +63,70 @@ socket.onmessage = function(e) {
     handle_data(method, data);
 };
 
+async function save_global_state(fileHandle){
+    const writable = await fileHandle.createWritable();
+    await writable.write(JSON.stringify(global_state));
+    await writable.close();
+}
+
+async function load_global_state(fileHandle){
+    const file = await fileHandle[0].getFile();
+    const text = await file.text();
+    global_state = Context.from_obj(JSON.parse(text));
+    update_charts();
+    update_series();
+}
+
+
+function add_button_methods(){
+    d3.select("#nav-data-load").on("click", function(){
+        const opts = {
+            types: [
+                {
+                    description: 'JSON File',
+                    accept: {
+                        'application/json': ['.json']
+                    }
+                },
+            ],
+            excludeAcceptAllOption: true,
+            multiple: false
+        };
+        window.showOpenFilePicker(opts)
+            .then(load_global_state)
+            .catch(console.log);
+    });
+    d3.select("#nav-data-save").on("click", function(){
+        const opts = {
+            types: [{
+                description: 'JSON File',
+                accept: {'application/json': ['.json']},
+            }],
+        };
+        window.showSaveFilePicker(opts)
+            .then(save_global_state)
+            .catch(console.log);
+    });
+    d3.select("#nav-data-clear-all").on("click", function(){
+        global_state.clear_charts();
+        global_state.clear_series();
+        update_charts();
+        update_series();
+    });
+    d3.select("#nav-data-clear-series").on("click", function(){
+        global_state.clear_series();
+        update_charts();
+        update_series();
+    });
+    d3.select("#nav-data-clear-charts").on("click", function(){
+        global_state.clear_charts();
+        update_charts();
+    });
+}
+
 window.onload = function() {
+    add_button_methods();
+
     // Try loading data from local storage.
     let context = window.localStorage.getItem("context");
     if (context === null) {
